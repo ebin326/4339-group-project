f0ba5fe559e65e457c9faee291d64d906c770325
ebin326 <71982537+ebin326@users.noreply.github.com>
2024-04-27 12:55:21 -0500

    removed hardcoded data and put back og code

--- frontend/src/components/barChart.vue
+++ frontend/src/components/barChart.vue
@@ -1,79 +1,61 @@
 <template>
-  <div class="chartbox">
-    <!-- canvas element for chart -->
-    <canvas id="attendanceChart"></canvas>
+  <div>
+    <canvas class="p-10" ref="attendanceChart"></canvas>
   </div>
 </template>
 
 <script>
 import { Chart, registerables } from 'chart.js'
 import { Colors } from 'chart.js';
-import barChartData from '../assets/attendancecode-data.js'
-
 Chart.register(...registerables)
 Chart.register(Colors);
 export default {
-  name: 'barChart',
-  data() {
-    return {
-      barChartData: barChartData
+  props: {
+    label: {
+      type: Array
+    },
+    chartData: {
+      type: Array
     }
   },
-  //establish Chart object after mounting the component
-  mounted() {
-    const ctx = document.getElementById('attendanceChart')
-    new Chart(ctx, this.barChartData)
+  async mounted() {
+    await new Chart(this.$refs.attendanceChart, {
+      type: 'bar',
+      data: {
+        labels: this.label,
+        datasets: [
+          {
+            borderWidth: 1,
+            data: this.chartData
+          }
+        ]
+      },
+      options: {
+        scales: {
+          y: {
+            ticks: {
+              stepSize: 1
+            }
+          },
+          x: {
+            gridLines: {
+              display: false
+            }
+          }
+        },
+        plugins: {
+          legend: {
+            display: false
+          },
+          title: {
+            display: true,
+            text: 'Attendance Chart'
+          }
+        },
+        responsive: true,
+        maintainAspectRatio: true
+      }
+    })
   }
 }
 </script>
-// export default {
-//   props: {
-//     label: {
-//       type: Array
-//     },
-//     chartData: {
-//       type: Array
-//     }
-//   },
-//   async mounted() {
-//     await new Chart(this.$refs.attendanceChart, {
-//       type: 'bar',
-//       data: {
-//         labels: this.label,
-//         datasets: [
-//           {
-//             borderWidth: 1,
-//             data: this.chartData
-//           }
-//         ]
-//       },
-//       options: {
-//         scales: {
-//           y: {
-//             ticks: {
-//               stepSize: 1
-//             }
-//           },
-//           x: {
-//             gridLines: {
-//               display: false
-//             }
-//           }
-//         },
-//         plugins: {
-//           legend: {
-//             display: false
-//           },
-//           title: {
-//             display: true,
-//             text: 'Attendance Chart'
-//           }
-//         },
-//         responsive: true,
-//         maintainAspectRatio: true
-//       }
-//     })
-//   }
-// }
-
-
